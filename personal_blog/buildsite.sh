b3ffa0fc98f1c00d08728727b07177b0c9cfd51e
Ryan-Lucey-13 <rlucey1300@gmail.com>
2024-04-23 13:01:01 -0400

    buildsite.sh into docs

--- personal_blog/buildsite.sh
+++ personal_blog/buildsite.sh
@@ -1,4 +1,4 @@
 cd personal_blog/
-cat templates/top.html content/aboutme.html templates/bottom.html > aboutme.html
-cat templates/top.html content/index.html templates/bottom.html > index.html
-cat templates/top.html content/projects.html templates/bottom.html > projects.html
+cat templates/top.html content/aboutme.html templates/bottom.html > ./docs/aboutme.html
+cat templates/top.html content/index.html templates/bottom.html > ./docs/index.html
+cat templates/top.html content/projects.html templates/bottom.html > ./docs/projects.html
