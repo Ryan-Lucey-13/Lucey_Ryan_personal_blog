7d9a7c5a425005d6d93c6f799ea733f3e4b745e7
Ryan-Lucey-13 <rlucey1300@gmail.com>
2024-04-30 17:52:55 -0400

    delete bash ssg

--- buildsite.sh
+++
@@ -1,4 +0,0 @@
-cd personal_blog/
-cat templates/top.html content/aboutme.html templates/bottom.html > ./docs/aboutme.html
-cat templates/top.html content/index.html templates/bottom.html > ./docs/index.html
-cat templates/top.html content/projects.html templates/bottom.html > ./docs/projects.html
