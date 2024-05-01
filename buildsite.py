d58a951c0a8c8bbefdd0f638e297eebebb56c041
Ryan-Lucey-13 <rlucey1300@gmail.com>
2024-05-01 16:46:00 -0400

    for loop in main()

--- buildsite.py
+++ buildsite.py
@@ -1,33 +1,28 @@
-"""def main():
-	top = open('./templates/top.html').read()
-	bottom = open('./templates/bottom.html').read()
-	#index
-	index = open('./content/index.html').read()
-	index_page = top + index + bottom
-	open('./docs/index.html', 'w+').write(index_page)
-	#projects
-	projects = open('./content/projects.html').read()
-	projects_page = top + projects + bottom
-	open('./docs/projects.html', 'w+').write(projects_page)
-	#aboutme
-	aboutme = open('./content/aboutme.html').read()
-	aboutme_page = top + aboutme + bottom
-	open('./docs/aboutme.html', 'w+').write(aboutme_page)
-main()"""
 pages = [
 	{
 	"filename": "content/index.html",
-	"output": "docs/index.html"
+	"output": "docs/index.html",
 	"title": "Blog",
 	},
 	{
 	"filename": "content/projects.html",
-	"output": "docs/projects.html"
+	"output": "docs/projects.html",
 	"title": "Projects",
 	},
 	{
 	"filename": "content/aboutme.html",
-	"output": "docs/aboutme.html"
+	"output": "docs/aboutme.html",
 	"title": "About Me",
 	},
 ]
+def main():
+	top = open('./templates/top.html').read()
+	bottom = open('./templates/bottom.html').read()
+	for page in pages:
+		filename = page['filename']
+		output_file = page['output']
+		index = open(filename).read()
+		index_page = top + index + bottom
+		open(output_file, 'w+').write(index_page)
+main()
+
