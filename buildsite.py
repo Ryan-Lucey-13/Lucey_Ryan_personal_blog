643a8098fabb3b028075246bdec26010c86fe001
Ryan-Lucey-13 <rlucey1300@gmail.com>
2024-05-01 13:33:19 -0400

    refactored buildsite.py into main()

--- buildsite.py
+++ buildsite.py
@@ -1,20 +1,17 @@
-#index
-top = open('./templates/top.html').read()
-index = open('./content/index.html').read()
-bottom = open('./templates/bottom.html').read()
-index_page = top + index + bottom
-open('./docs/index.html', 'w+').write(index_page)
+def main():
+	top = open('./templates/top.html').read()
+	bottom = open('./templates/bottom.html').read()
+	#index
+	index = open('./content/index.html').read()
+	index_page = top + index + bottom
+	open('./docs/index.html', 'w+').write(index_page)
+	#projects
+	projects = open('./content/projects.html').read()
+	projects_page = top + projects + bottom
+	open('./docs/projects.html', 'w+').write(projects_page)
+	#aboutme
+	aboutme = open('./content/aboutme.html').read()
+	aboutme_page = top + aboutme + bottom
+	open('./docs/aboutme.html', 'w+').write(aboutme_page)
+main()
 
-#projects
-top = open('./templates/top.html').read()
-projects = open('./content/projects.html').read()
-bottom = open('./templates/bottom.html').read()
-projects_page = top + projects + bottom
-open('./docs/projects.html', 'w+').write(projects_page)
-
-#aboutme
-top = open('./templates/top.html').read()
-aboutme = open('./content/aboutme.html').read()
-bottom = open('./templates/bottom.html').read()
-aboutme_page = top + aboutme + bottom
-open('./docs/aboutme.html', 'w+').write(aboutme_page)
